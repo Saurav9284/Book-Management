965f23291ea20cdcc05302c9e54618e157fb34e0
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-04-25 18:08:49 +0530

    Model for book and user added

---
+++ src/Models/book.model.js
@@ -0,0 +1,21 @@
+
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  publicationYear: {
+    type: Number,
+    required: true
+  }
+});
+
+const BookModel = mongoose.model('Book', bookSchema);
+
+module.exports = BookModel
