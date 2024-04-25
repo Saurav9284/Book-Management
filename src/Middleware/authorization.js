d78795e9e432f9a42c4b8d7844090c5ec2ef2ecc
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-04-25 19:52:23 +0530

    middleware

---
+++ src/Middleware/authorization.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../Config/db')
+
+const authorization = (req,res,next) => {
+
+    if (!req.headers.authorization) {
+        return res.send({ msg: 'Login first' });
+    }
+    const token = req.headers.authorization.split("Bearer ")[1];
+
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.send({ msg: 'Please login first!' });
+        }
+        req.userId = decoded.userId;
+
+        next();
+    });
+}
+
+module.exports == authorization
